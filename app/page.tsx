f027d312b5763051452eaedc0a1106bb7d22c1f1
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-02-20 14:17:48 +0100

    centering text on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
-  return (
-    
-    <main>
+  return (    
+    <main className="absolute inset-0 flex items-center justify-center">
       Aplikacja w budowie
     </main>
   );
