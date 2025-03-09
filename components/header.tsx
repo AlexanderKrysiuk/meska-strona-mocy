2a4fddd36e83257bd758e557fe9d10d02d87f94c
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-03-09 16:03:59 +0100

    menu for mobile

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,12 @@
 "use client"
-import { Avatar, Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+import { Avatar, Button, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@heroui/react";
 import { ThemeSwitcher } from "./theme-switcher";
 import { useCurrentUser } from "@/hooks/user";
 import { usePathname } from "next/navigation";
 import { Role } from "@prisma/client";
 import { ModeratorItems } from "./user-menu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { signOut } from "next-auth/react";
 
 const Header = () => {
@@ -45,7 +45,7 @@ const Header = () => {
                                 <Avatar
                                     showFallback
                                     src={user.image!}
-                                    className="cursor-pointer hover:ring-2 hover:ring-primary transition-all duration-300"
+                                    className="cursor-pointer hover:ring-2 hover:ring-primary transition-all duration-400"
                                 />
                             </DropdownTrigger>
                             <DropdownMenu>
@@ -91,7 +91,60 @@ const Header = () => {
                         </Button>
                     } 
                 </NavbarItem>
+                <NavbarMenuToggle className="lg:hidden"/>
             </NavbarContent>
+            <NavbarMenu>
+                {user && (
+                    <NavbarMenuItem>
+                        <div className="flex justify-between items-center mb-1">
+                            Witaj {user.name}
+                            <Avatar
+                                size="sm"
+                                showFallback
+                                src={user.image!}
+                            />
+                        </div>
+                        <Divider/>
+                    </NavbarMenuItem>
+                )}
+                {user?.role === Role.Moderator || user?.role === Role.Admin && 
+                    <div>
+                        <span className="text-sm text-foreground-500">
+                            Moderator
+                        </span>
+                        {ModeratorItems.map((item)=>(
+                            <NavbarMenuItem
+                                key={item.title}
+                            >
+                                <Link
+                                    href={item.href}
+                                    color={pathname.startsWith(item.href) ? "primary" : "foreground"}
+                                    className="flex gap-2 hover:primary transition-colors duration-400"
+                                >
+                                </Link>
+                            </NavbarMenuItem>
+                        ))}
+                        <Divider/>
+                    </div>
+                }
+                <NavbarMenuItem>
+                    {user ? 
+                        <Link
+                            onPress={()=>{signOut()}}
+                            color="danger"
+                            className="cursor-pointer"
+                        >
+                            <FontAwesomeIcon icon={faArrowRightFromBracket} className="mr-2"/> Wyloguj
+                        </Link>
+                        :
+                        <Link
+                            href="/auth/start"
+                        >
+                            <FontAwesomeIcon icon={faArrowRightToBracket} className="mr-2"/> Start
+                        </Link>
+                    }
+                </NavbarMenuItem>
+            </NavbarMenu>
         </Navbar>
      );
 }
