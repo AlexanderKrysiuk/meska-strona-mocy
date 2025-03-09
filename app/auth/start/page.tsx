44b3a0d3a902e52a2f1f978a97e17c4ee8162c5a
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-03-09 17:07:29 +0100

    padding fix

--- app/auth/start/page.tsx
+++ app/auth/start/page.tsx
@@ -6,8 +6,8 @@ import { Card, CardBody, Tab, Tabs } from "@heroui/react";
 
 const StartPage = () => {
     return ( 
-        <main className="absolute flex w-full justify-center mt-[0.2vh]">
-            <Card className="w-full max-w-xs m-4">
+        <main className="absolute flex w-full justify-center mt-[0.2vh] p-4">
+            <Card className="w-full max-w-xs">
                 <CardBody>
                     <Tabs fullWidth>
                         <Tab title="Logowanie">
--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "@heroui/react";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white pt-4 mt-auto pb-16 lg:pb-4">
+        <footer className="bg-gray-800 text-white pt-4 mt-auto pb-16 lg:pb-4 px-4">
             <div className="max-w-screen-xl mx-auto text-center">
                 <p>© 2025 Męska Strona Mocy. Wszystkie prawa zastrzeżone.</p>
                 <Link href="/regulaminy" className="text-white">
