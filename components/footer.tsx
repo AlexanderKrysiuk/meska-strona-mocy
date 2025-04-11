cddb432e6cb5d9afdb63abf1cc620d319a52d8bf
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-04-11 18:48:54 +0200

    separation regulations clean up

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,11 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white pt-4 mt-auto pb-16 lg:pb-4">
             <div className="max-w-screen-xl mx-auto text-center">
-                <p>© 2025 Męska Strona Mocy. Wszystkie prawa zastrzeżone.</p>
-                <Link href="/regulamin" className="text-white">
-                    Regulamin
-                </Link>
+                <p className="text-white text-lg">© 2025 Męska Strona Mocy. Wszystkie prawa zastrzeżone. {" "}
+                    <Link href="/regulaminy" size="lg" className="inline">
+                        Regulaminy
+                    </Link>
+                </p>
             </div>
         </footer>    
     );
