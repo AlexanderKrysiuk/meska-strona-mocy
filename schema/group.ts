9ff2e86a5dea1dbc3fdb93ac9b96117c723db75a
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-07-23 19:07:28 +0200

    group schema cleanup

--- schema/group.ts
+++ schema/group.ts
@@ -18,54 +18,9 @@ const slug = z.string()
     message: "Slug nie może wyglądać jak identyfikator systemowy (UUID)"
   })
 
-  //.refine(
-  //  (value) => /^[a-z0-9-]+$/.test(value),
-  //  {
-  //    message: "Unikalny odnośnik może zawierać tylko małe litery, cyfry i myślniki"
-  //  }
-  //)
-  //.refine(
-  //  (value) => !value.startsWith("-") && !value.endsWith("-"),
-  //  {
-  //    message: "Unikalny odnośnik nie może zaczynać się ani kończyć myślnikiem"
-  //  }
-  //)
 const street = z.string().trim().max(255, "Adres jest zbyt długi").nullable()
 
 const cityId = z.string().uuid().nullable()
-
-// const price = z.union([
-//   z.number().refine(val => val === 0 || val >= 10, {
-//     message: "Cena musi wynosić 0 (darmowe spotkanie) lub minimum 10 zł"
-//   }),
-//   z.null()
-// ]).optional()
-
-// const price = z.number()
-//   .transform((val) => (isNaN(val) ? null : val))
-//   .refine((val) => val === 0 || val >= 10, {
-//     message: "Cena musi wynosić 0 (darmowe spotkanie) lub minimum 10 zł"
-//   })
-//   .nullable()
-
-// const price = z.preprocess(
-//   (val) => {
-//     if (typeof val === "number") {
-//       return isNaN(val) ? null : val;
-//     }
-//     if (typeof val === "string") {
-//       const num = Number(val);
-//       return isNaN(num) ? null : num;
-//     }
-//     return val;
-//   },
-//   z
-//     .number()
-//     .nullable()
-//     .refine((val) => val === null || val === 0 || val >= 10, {
-//       message: "Cena musi wynosić 0 (darmowe spotkanie) lub minimum 10 zł"
-//     })
-// );
 
 const price = z.number()
   .nullable()
