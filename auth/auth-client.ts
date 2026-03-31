aa74b39e070ffa88aab46d7cf3607f028d044639
Alexander Krysiuk <alex@mini-alexander.home>
2026-03-31 19:15:43 +0200

    baseURL betterauth change

--- auth/auth-client.ts
+++ auth/auth-client.ts
@@ -6,7 +6,7 @@ import { ac, admin, moderator } from "./permissions";
 
 const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_APP_URL,
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [
         inferAdditionalFields<typeof auth>(),
         adminClient({
